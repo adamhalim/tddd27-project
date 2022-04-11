7c6e4529a3ef8bbefe27c8e110a939e8704e425f
Adam Halim <adaab301@student.liu.se>
2022-04-11 11:59:37 +0000

    frontend: Add / Route & Home component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home/';
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+    </Router>
   );
 }
 
