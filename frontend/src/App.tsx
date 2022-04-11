e986ed20e1d5e299bd14109ac9023bab5683e386
Adam Halim <adaab301@student.liu.se>
2022-04-11 12:21:23 +0000

    Frontend: add <Header/> Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header';
 import Home from './pages/Home';
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />}/>
       </Routes>
