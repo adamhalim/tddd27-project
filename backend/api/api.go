5184c753bddc1be525fdb75172b731666b8f7abc
Adam Halim <adaab301@student.liu.se>
2022-04-11 17:52:51 +0000

    Backend: refactor main to api/

---
+++ backend/api/api.go
@@ -0,0 +1,17 @@
+package api
+
+import "github.com/gin-gonic/gin"
+
+func Start() {
+	handleRequests()
+}
+
+func handleRequests() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
