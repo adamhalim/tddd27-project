98a417ac860ace3e6fa3c7b6a6987aeafeafbe3b
Adam Halim <adaab301@student.liu.se>
2022-04-21 17:59:44 +0000

    <ProgressBar/>: Add animation to progress

--- frontend/src/components/UploadProgress/ProgressBar/ProgressBar.tsx
+++ frontend/src/components/UploadProgress/ProgressBar/ProgressBar.tsx
@@ -6,15 +6,14 @@ const ProgressBar = ({ progress }: ProgressBarType) => {
     const wrapperStyle = {
         height: '100%',
         width: `${progress}%`,
-        backgroundColor: '#558B2F',
         textAlign: 'right' as 'right',
-        borderRadius: 10
+        borderRadius: 10,
     }
 
     return (
         <div className="progress-bar-container">
-            <div style={wrapperStyle}>
-                <span>{`${progress.toFixed(0)}%`}</span>
+            <div className='progress' style={wrapperStyle}>
+                <span className=''>{`${progress.toFixed(0)}%`}</span>
             </div>
         </div>
     )
