6242083e25aacab0ae43db549f443cbb39f7b2b6
Adam Halim <adaab301@student.liu.se>
2022-04-22 14:46:11 +0000

    Add lib/postgres package

---
+++ lib/postgres/go.mod
@@ -0,0 +1,10 @@
+module gitlab.liu.se/adaab301/tddd27_2022_project/lib/postgres
+
+go 1.18
+
+require github.com/lib/pq v1.10.5
+
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
---
+++ lib/postgres/postgres.go
@@ -0,0 +1,128 @@
+package postgres
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var (
+	port   string
+	host   string
+	user   string
+	dbName string
+
+	db *sqlx.DB
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	port = os.Getenv("POSTGRES_PORT")
+	if port == "" {
+		log.Fatal("no POSTGRES_PORT in .env")
+	}
+	host = os.Getenv("POSTGRES_HOST")
+	if host == "" {
+		log.Fatal("no POSTGRES_HOST in .env")
+	}
+	user = os.Getenv("POSTGRES_USER")
+	if user == "" {
+		log.Fatal("no POSTGRES_USER in .env")
+	}
+	dbName = os.Getenv("POSTGRES_DBNAME")
+	if dbName == "" {
+		log.Fatal("no POSTGRES_DBNAME in .env")
+	}
+	initDB()
+}
+
+func initDB() {
+	connString := fmt.Sprintf("port=%s host=%s user=%s dbname=%s sslmode=disable", port, host, user, dbName)
+	database, err := sqlx.Open("postgres", connString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = database
+	createTables()
+}
+
+func createTables() {
+	_, err := db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			uid VARCHAR(50) NOT NULL,
+			CONSTRAINT pk_uid PRIMARY KEY(uid)
+		)
+	`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS videos (
+			chunkname VARCHAR(36) NOT NULL,
+			lastViewed NUMERIC(11,0) NOT NULL,
+			uid VARCHAR(50) NOT NULL,
+			FOREIGN KEY(uid) REFERENCES users(uid),
+			CONSTRAINT pk_chunkname PRIMARY KEY(chunkname)
+		)
+	`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func AddUser(user User) error {
+	stmt, err := db.Prepare(`
+		INSERT INTO users(uid) VALUES($1)
+	`)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(user.Uid)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func FindUser(uid string) User {
+	var user User
+	row := db.QueryRow(`SELECT uid FROM users WHERE uid=$1`, uid)
+	err := row.Scan(user.Uid)
+	if err != nil {
+		return User{}
+	}
+	return user
+}
+
+func UserExists(uid string) bool {
+	user := FindUser(uid)
+	return user == User{}
+}
+
+func AddVideo(video Video) error {
+	stmt, err := db.Prepare(`
+		INSERT INTO videos(
+			chunkname,
+			lastviewed,
+			uid)
+			VALUES($1, $2, $3)
+	`)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(video.Chunkname, video.LastViewed, video.Uid)
+	if err != nil {
+		return err
+	}
+	return nil
+}
