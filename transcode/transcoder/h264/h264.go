15e07986da4b106b80b1ce0a14644f5e817fb2ac
Adam Halim <adaab301@student.liu.se>
2022-06-01 08:35:53 +0000

    transcoder: TranscodeToh264 doesn't upload to S3 storage

--- transcode/transcoder/h264/h264.go
+++ transcode/transcoder/h264/h264.go
@@ -2,6 +2,7 @@ package h264
 
 import (
 	"fmt"
+	"os"
 
 	ffmpeg_go "github.com/u2takey/ffmpeg-go"
 	"gitlab.liu.se/adaab301/tddd27_2022_project/lib/fileutil"
@@ -9,14 +10,15 @@ import (
 )
 
 func TranscodeToh264(fileName string, originalFileName string, dir string, uid string) error {
-	outputFileName := fmt.Sprintf("%s/%s.mp4", dir, fileutil.RemoveFileExtension(originalFileName))
+	os.MkdirAll(fmt.Sprintf("%s/transcoded/", dir), os.ModeDir)
+	outputFileName := fmt.Sprintf("%s/transcoded/%s.mp4", dir, fileutil.RemoveFileExtension(originalFileName))
 	ffmpeg_go.Input(fileName).
 		Output(outputFileName, ffmpeg_go.KwArgs{"c:v": "libx264"}).
 		OverWriteOutput().
 		ErrorToStdOut().
 		Run()
 
-	dirName := fileutil.RemoveFileNameFromDirectory(dir)
-	objectstore.FiletoDB(dirName+"/"+"video"+".mp4", outputFileName, originalFileName, uid)
+	return nil
+}
 	return nil
 }
