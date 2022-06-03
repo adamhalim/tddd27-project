aac3f26889453df34520d477acd122b4fef453ac
Adam Halim <adaab301@student.liu.se>
2022-06-03 11:44:37 +0000

    frontend refactor: moved isAuthenticated from <Header/> to <AuthButtons/>

--- frontend/src/components/Header/AuthButtons/AuthButtons.tsx
+++ frontend/src/components/Header/AuthButtons/AuthButtons.tsx
@@ -2,8 +2,11 @@ import { useAuth0 } from "@auth0/auth0-react"
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 
-const AuthButtons = () => {
-    const { isAuthenticated } = useAuth0();
+type AuthButtonsType = {
+    isAuthenticated: boolean,
+}
+
+const AuthButtons = ({isAuthenticated}: AuthButtonsType) => {
 
     return isAuthenticated ? <LogoutButton /> : <LoginButton />
 }
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,9 +1,12 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import { Link } from 'react-router-dom'
 import AuthButtons from './AuthButtons/'
 import './style.css'
 
 
 const Header = () => {
+    const { isAuthenticated } = useAuth0();
+
     return (
         <header>
             <div className="header-left">
@@ -12,7 +15,9 @@ const Header = () => {
             <Link to="/"><h1 className='text-4xl' >vidds™</h1></Link>
             </div>
             <div className="header-right">
-                <AuthButtons />
+                <AuthButtons 
+                    isAuthenticated={isAuthenticated}
+                />
             </div>
         </header>
     )
